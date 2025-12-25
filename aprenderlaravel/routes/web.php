d79faf65543dc8f02922ace2be45c00c7bf2e9a6
darkking019 <12jonathanribe@gmail.com>
2025-12-25 13:53:57 -0300

    add

--- aprenderlaravel/routes/web.php
+++ aprenderlaravel/routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/pagamento/sucesso', fn () => 'PAGAMENTO APROVADO');
 Route::get('/pagamento/erro', fn () => 'PAGAMENTO ERRO');
 Route::get('/pagamento/pendente', fn () => 'PAGAMENTO PENDENTE');
-
-
-
+Route::get('/health', function () {
+    return response()->json([
+        'app' => 'ok',
+        'env' => app()->environment(),
+    ]);
+});
 Route::get('/', fn () => ['Laravel' => app()->version()]);
 
 
