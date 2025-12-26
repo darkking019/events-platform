b71f579055bbe1e945de7b48bb904f704f2ae812
darkking019 <12jonathanribe@gmail.com>
2025-12-26 00:04:17 -0300

    add

--- front-next/app/lib/api.ts
+++ front-next/app/lib/api.ts
@@ -1,6 +1,17 @@
+import { useAuth } from "../context/AuthContext";
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
-  withCredentials: true,
-});
+export const useApi = () => {
+  const { token } = useAuth();
+
+  const api = axios.create({
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    headers: token
+      ? {
+          Authorization: `Bearer ${token}`,
+        }
+      : undefined,
+  });
+
+  return api;
+};
